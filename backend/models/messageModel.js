c45bb0f3f15dc14e945403655a0629a7337a973e
HungTran2101 <89181217+HungTran2101@users.noreply.github.com>
2023-03-27 15:55:37 +0700

    Update messageModel.js

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -6,11 +6,7 @@ const messageSchema = mongoose.Schema(
     roomId: { type: mongoose.Schema.Types.ObjectId, ref: "Rooms" },
     senderId: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
     msg: { type: String, trim: true, default: "" },
-    files: [
-      {
-        fileId: { type: mongoose.Schema.Types.ObjectId, ref: "Files" },
-      },
-    ],
+    fileIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Files" }],
     unSend: { type: Boolean, default: false },
     deleted: { type: Boolean, default: false },
   },
