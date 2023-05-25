e1205f2680d12a50354c512951c65da671376299
HungTran2101 <89181217+HungTran2101@users.noreply.github.com>
2023-05-25 15:13:12 +0700

    Update useIsMobile.tsx

--- frontend/src/components/Meeting/hooks/useIsMobile.tsx
+++ frontend/src/components/Meeting/hooks/useIsMobile.tsx
@@ -1,6 +1,6 @@
 import { useMediaQuery } from "react-responsive";
 
-const useIsMobile = (maxWidth) => {
+const useIsMobile = (maxWidth?) => {
   const isMobile = useMediaQuery({ maxWidth: maxWidth || 767 });
   return isMobile;
 };
