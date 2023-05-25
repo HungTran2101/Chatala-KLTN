60ceb31c37bb5dfa92e4fa6b470be93c22efdd0f
HungTran2101 <89181217+HungTran2101@users.noreply.github.com>
2023-05-25 13:56:05 +0700

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,18 +1,21 @@
-const Users = require("../models/userModel");
-const { decodeJWT } = require("../utils/utilFunctions");
-const ErrorHandler = require("../utils/errorHandler");
+const Users = require('../models/userModel');
+const { decodeJWT } = require('../utils/utilFunctions');
+const ErrorHandler = require('../utils/errorHandler');
 
 module.exports = async (req, res, next) => {
+  const { token } = req.signedCookies;
 
-    const { token } = req.signedCookies;
+  if (!token) {
+    return next(new ErrorHandler('Unauthorized!', 401));
+  }
 
-    if(!token) {
-        return next(new ErrorHandler("Unauthorized!", 401));
-    }
+  const isDecoded = decodeJWT(token);
 
-    const isDecoded = decodeJWT(token);
+  req.user = await Users.findById(isDecoded.id);
 
-    req.user = await Users.findById(isDecoded.id);
+  if (!req.user) {
+    return next(new ErrorHandler('Unauthorized!', 401));
+  }
 
-    next();
-}
+  next();
+};
